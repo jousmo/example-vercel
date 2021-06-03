51186ad35ee944dd38c086bb69692593ffbfb1a7
Jose Uscanga <jousmo@outlook.com>
2021-06-03 09:06:07 -0500

    Configure Babel Loader With Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,19 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   }
 }
